e93e79f0b7bbf2bfbd3ff0c1b495966c45e4bdaa
Jackson Tian <shyvo1987@gmail.com>
2013-06-25 14:48:40 +0800

    Initial Commit

---
+++ Browser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Browser
+//
+//  Created by JacksonTian on 13-6-25.
+//  Copyright (c) 2013年 wanshanglaiwojiachifan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Browser/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Browser
+//
+//  Created by JacksonTian on 13-6-25.
+//  Copyright (c) 2013年 wanshanglaiwojiachifan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
