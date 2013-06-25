3259d87bb5c999c5777a94345cc42a6e3d1c43f4
Jackson Tian <shyvo1987@gmail.com>
2013-06-25 22:24:33 +0800

    实现浏览器第一版

--- Browser/ViewController.m
+++ Browser/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+- (IBAction)go:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *address;
+@property (weak, nonatomic) IBOutlet UIWebView *browser;
 
 @end
 
@@ -18,12 +21,39 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.browser.scalesPageToFit = YES;
+    self.browser.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)go:(id)sender {
+    [self.address resignFirstResponder];
+    NSString *urlString = self.address.text;
+    NSLog(@"%@", urlString);
+    [self.address resignFirstResponder];
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *request =[NSURLRequest requestWithURL:url];
+    [self.browser loadRequest:request];
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)webView
+{
+    NSLog(@"start load");
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    NSLog(@"finished");
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+{
+    NSLog(@"%@", error.description);
 }
 
 @end
